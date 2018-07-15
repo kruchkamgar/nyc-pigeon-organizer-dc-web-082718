c1268e5fd7559dc85017bdb12e6115d8c87ab291
kruchkamgar <kruchkamgar@gmail.com>
2018-07-14 20:20:57 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+pigeon_list = {}
+
+  data.each { | facet, h_values |
+    h_values.each { | value, a_names |
+      a_names.each { | name |
+        if pigeon_list[name] == nil then pigeon_list[name] = {} end
+        # pigeon_list[name].merge!({facet => value})
+        if pigeon_list[name][facet] == nil then pigeon_list[name][facet] = [] end
+        pigeon_list[name][facet] << value.to_s
+      }
+    }
+  }
+  # binding.pry
+  pigeon_list
 end
